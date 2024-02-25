397f794effcef5a8c710aac4d735674f48c8fbca
ismayel-shekh <md.shekh.ismayel@gmail.com>
2024-02-25 17:58:38 +0600

    first commit

--- quiz_history/urls.py
+++ quiz_history/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from . import views
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register('', views.quiz_historyViewset)
 urlpatterns = [
     path('', include(router.urls))
